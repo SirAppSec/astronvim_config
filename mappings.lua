ff3ed944092bdc3def6f6202cc965538ba764806
Amit Srour <asrour@paypal.com>
2024-01-08 21:43:20 +0200

    stable - before fixing pipenv interpreter

--- mappings.lua
+++ mappings.lua
@@ -30,7 +30,9 @@ return {
     -- tables with the `name` key will be registered with which-key if it's installed
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
-    -- quick save
+    ["<leader>a"] = {"<cmd>echo 'SirAppSec'<cr>",desc = "Trying out the command echo"},
+    
+    -- a quick save
     -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   t = {
